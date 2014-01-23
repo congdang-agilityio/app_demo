0671c0326aef172e73ecbd85f12ebd69d99c59f5
vagrant <vagrant@precise64.(none)>
2014-01-23 16:15:50 +0000

    init rspec

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-AppDemo::Application.config.secret_key_base = 'f0b165aa3291cdc3c4767cb5c85f89fe5f3659e2d79568d18f56a071cca3c3f20f48d5a6a80f778a3f6741faea637c4581737b087a7a87e8e92f579f0cb60781'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+AppDemo::Application.config.secret_key_base = secure_token
